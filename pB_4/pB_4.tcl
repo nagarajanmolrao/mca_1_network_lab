e701025638c45846cbe6e0092f77fb58cc51b139
N Anmol <49219245+nagarajanmolrao@users.noreply.github.com>
2021-03-10 08:10:23 +0530

    4th and 5th programs

---
+++ pB_4/pB_4.tcl
@@ -0,0 +1,51 @@
+set ns [new Simulator -multicast on];
+
+set trace [open pB_4.tr w]
+$ns trace-all $trace-all
+
+set namtrace [open pB_4.nam w]
+$ns namtrace-all $namtrace
+
+set group [Node allocaddr];
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+
+$ns duplex-link $n0 $n1 1.5Mb 10ms DropTail
+$ns duplex-link $n0 $n2 1.5Mb 10ms DropTail
+
+set mproto DM;
+set mrthandle [$ns mrtproto $mproto];
+
+set udp [new Agent/UDP]
+$ns attach-agent $n0 $udp
+
+set src [ new Application/Traffic/CBR]
+$src attach-agent $udp
+
+$udp set dst_addr_ $group
+$udo set dst_port_ 0
+
+set rcvr1 [new Agnet/LossMonitor]
+$ns attach-agent $n1 $rcvr1
+
+set rcvr2 [new Application/LossMonitor]
+$ns attach-agent $n2 $rcvr2
+
+$ns at 0.3 "$n2 join-group $rcvr2 $group"
+$ns at 2.0 "$src start"
+$ns at 3.3 "$n2 leave-group rcvr2 $group"
+$ns at 5.0 "$src stop"
+
+proc finish { } {
+global ns namtrace trace
+$ns flush-trace
+close $namtrace
+close $trace
+exec nam pB_4.nam &
+exit 0
+}
+
+$ns at 10.0 "finish"
+$ns run
+
---
+++ pB_5/pB_5.tcl
@@ -0,0 +1,56 @@
+set ns [new Simulator]
+
+set nf [open pB_5.nam w]
+$ns namtrace-all $nf
+
+set nt [open pB_5.tr w]
+$ns trace-all $nt
+
+set n0 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+
+$ns duplex-link $n0 $n2 1Mb 10ms DropTail
+$ns duplex-link $n1 $n2 1Mb 10ms DropTail
+$ns duplex-link $n2 $n3 1Mb 10ms DropTail
+
+$ns queue-limit $n0 $n2 50
+$ns queue-limit $n1 $n2 50
+$ns queue-limit $n2 $n3 50
+
+set tcp0 [new Agent/TCP]
+$ns attach-agent $n0 $tcp0
+Agent/TCP set packetSize_ 1000
+
+set ftp0 [new Application/FTP]
+$ftp0 attach-agent $tcp0
+
+
+set sink1 [new Agent/TCPSink]
+$ns attach-agent $n3 $sink1
+
+set tcp1 [new Agent/TCP]
+$ns attach-agent $n1 $tcp1
+
+$ns connect $tcp1 $sink1
+
+set telnet0 [new Application/Telnet]
+$telnet0 set interval_ 0.005
+$telnet0 attach-agent $tcp1
+
+proc finish { } {
+global ns nf nt
+$ns flush-trace
+close $nf
+close $nt
+exec nam pB_5.nam &
+exec awk -f pB_5.awk pB_5.tr &
+exit 0
+}
+
+$ns at 0.75 "$ftp0 start"
+$ns at 0.5 "$telnet0 start"
+$ns at 4.5 "$telnet0 stop"
+$ns at 4.75 "$ftp0 stop"
+$ns at 5.0 "finish"
+$ns run
